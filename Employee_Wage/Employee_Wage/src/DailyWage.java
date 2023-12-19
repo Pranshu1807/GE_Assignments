1a475dc6c7ddf60a62332b58fdcd917e608d43db
Pranshu Srivastava <pranshusrivastava000@gmail.com>
2023-12-20 00:00:56 +0530

    DailyWages added

--- Employee_Wage/Employee_Wage/src/DailyWage.java
+++ Employee_Wage/Employee_Wage/src/DailyWage.java
@@ -1,5 +1,14 @@
+import java.lang.Math;
+
 public class DailyWage {
     public static void main(String[] args) {
-        System.out.println(20 * 8);
+        int WagePerHour = 20;
+        int noOfHour = 8;
+        double rand = Math.random();
+        int randomNumber = (int) (rand * 100);
+        boolean present;
+        present = randomNumber % 2 == 0 ? true : false;
+        int TotalDailyWage = present == true ? WagePerHour * noOfHour : 0;
+        System.out.println("Daily Wage of the Employee is" + TotalDailyWage);
     }
 }
