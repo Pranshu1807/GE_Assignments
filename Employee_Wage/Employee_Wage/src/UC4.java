ecc52080e817e6fbdd0ef752d83f321783b87eaf
Pranshu Srivastava <pranshusrivastava000@gmail.com>
2023-12-20 00:54:02 +0530

    Switch Case Added

---
+++ Employee_Wage/Employee_Wage/src/UC4.java
@@ -0,0 +1,27 @@
+import java.lang.Math;
+
+public class UC4 {
+    public static void main(String[] args) {
+        double rand = Math.random() * 100;
+        int randomNumber = (int) rand;
+        int fullTimeHour = 8;
+        int hourlyWage = 20;
+        int partTimeHour = 5;
+        int dailyWage;
+        switch (randomNumber % 3) {
+            case 1: {
+                dailyWage = fullTimeHour * hourlyWage;
+                break;
+            }
+            case 2: {
+                dailyWage = partTimeHour * hourlyWage;
+                break;
+            }
+            default:
+                dailyWage = 0;
+                break;
+        }
+        System.out.println(" Daily Wage of the customer is " + dailyWage);
+    }
+
+}
