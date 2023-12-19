e203b888b59e57025c2f6e83f1a1fbbed0448f6b
Pranshu Srivastava <pranshusrivastava000@gmail.com>
2023-12-20 00:48:31 +0530

    Part Time Added

---
+++ Employee_Wage/Employee_Wage/src/UC3.java
@@ -0,0 +1,21 @@
+import java.lang.Math;
+
+public class UC3 {
+    public static void main(String[] args) {
+        double rand = Math.random() * 100;
+        int randomNumber = (int) rand;
+        int fullTimeHour = 8;
+        int hourlyWage = 20;
+        int partTimeHour = 5;
+        boolean isFullTime = (randomNumber % 3 == 1);
+        boolean isPartTime = (randomNumber % 3 == 2);
+        int dailyWage = 0;
+        if (isFullTime) {
+            dailyWage = fullTimeHour * hourlyWage;
+        }
+        if (isPartTime) {
+            dailyWage = partTimeHour * hourlyWage;
+        }
+        System.out.println("Daily Wage of the customer is " + dailyWage);
+    }
+}
