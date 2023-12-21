c759fee7d41535ce5b9d821530e5597d1e32c3cb
Pranshu1807 <88945865+Pranshu1807@users.noreply.github.com>
2023-12-21 10:39:02 +0530

    Update and rename present.java to Start.java

--- Employee_Wage/Employee_Wage/src/Start.java
+++ Employee_Wage/Employee_Wage/src/Start.java
@@ -1,7 +1,7 @@
 import java.lang.Math;
 
 public class present {
-    public static void main(String[] args) {
+    public static void Start.(String[] args) {
         double rand = Math.random();
         int randomNumber = (int) (rand * 100);
         boolean present;
