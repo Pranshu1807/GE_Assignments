3a171a793657ce878f852655fd6ee6384ab5aab9
Pranshu Srivastava <pranshusrivastava000@gmail.com>
2023-12-25 22:10:21 +0530

    changes in UC8

--- Employee_Wage/Employee_Wage/src/Start.java
+++ Employee_Wage/Employee_Wage/src/Start.java
@@ -74,45 +74,56 @@ class Start {
         CompanyDetails Employee1CompanyDetails = new CompanyDetails();
         ArrayList<Company> Employee1Companies = Employee1CompanyDetails.companies;
 
-        System.out.println("Enter the value corresponding to the details you want");
-        System.out.println("Enter 1 for checking attendance");
-        System.out.println("Enter 2 for calculating daily Wage");
-        System.out.println("Enter 3 for calculating monthly Wage");
-        System.out.println("Enter 4 for calculating monthly Wage with max hours limitation");
+        boolean end = false;
+        while (true) {
+            System.out.println("Enter the value corresponding to the details you want");
+            System.out.println("Enter 0 to end");
+            System.out.println("Enter 1 for checking attendance");
+            System.out.println("Enter 2 for calculating daily Wage");
+            System.out.println("Enter 3 for calculating monthly Wage");
+            System.out.println("Enter 4 for calculating monthly Wage with max hours limitation");
 
-        int choice = sc.nextInt();
+            int choice = sc.nextInt();
 
-        switch (choice) {
-            case 1: {
-                Present(Employee1Companies);
-                break;
-            }
-            case 2: {
-                for (int i = 0; i < Employee1Companies.size(); i++) {
-                    int DailyWage = dailyWage(Employee1Companies.get(i));
-                    System.out.println("Daily Wage of the Employee in company " + (i + 1) + " is " + DailyWage);
+            switch (choice) {
+                case 0: {
+                    end = true;
+                    break;
                 }
-                break;
-            }
-            case 3: {
-                for (int i = 0; i < Employee1Companies.size(); i++) {
-                    int MonthlyWage = monthlyWage(Employee1Companies.get(i));
-                    System.out.println("Monthly Wage of the Employee in company " + (i + 1) + " is " + MonthlyWage);
+                case 1: {
+                    Present(Employee1Companies);
+                    break;
                 }
-                break;
-            }
-            case 4: {
-                for (int i = 0; i < Employee1Companies.size(); i++) {
-                    int MonthlyWageWithLimitation = maxHoursAndDays(Employee1Companies.get(i));
-                    System.out.println("Monthly Wage of the Employee in company " + (i + 1) + " with hours limit is "
-                            + MonthlyWageWithLimitation);
+                case 2: {
+                    for (int i = 0; i < Employee1Companies.size(); i++) {
+                        int DailyWage = dailyWage(Employee1Companies.get(i));
+                        System.out.println("Daily Wage of the Employee in company " + (i + 1) + " is " + DailyWage);
+                    }
+                    break;
                 }
-                break;
+                case 3: {
+                    for (int i = 0; i < Employee1Companies.size(); i++) {
+                        int MonthlyWage = monthlyWage(Employee1Companies.get(i));
+                        System.out.println("Monthly Wage of the Employee in company " + (i + 1) + " is " + MonthlyWage);
+                    }
+                    break;
+                }
+                case 4: {
+                    for (int i = 0; i < Employee1Companies.size(); i++) {
+                        int MonthlyWageWithLimitation = maxHoursAndDays(Employee1Companies.get(i));
+                        System.out
+                                .println("Monthly Wage of the Employee in company " + (i + 1) + " with hours limit is "
+                                        + MonthlyWageWithLimitation);
+                    }
+                    break;
+                }
+                default: {
+                    System.out.println("Enter a value between 1-4");
+                    break;
+                }
             }
-            default: {
-                System.out.println("Enter a value between 1-4");
+            if (end)
                 break;
-            }
         }
 
     }
